62b71bf2d51d181870e2da9d9bb42c5533d7d54e
Victor <victorlrmatos@gmail.com>
2023-06-25 18:30:01 -0300

    button text become white when hover

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -6,8 +6,10 @@ export const Button = ({ label, onClick, customStyles, ...props }) => {
       onClick={onClick}
       className={`dark:hover:text-dark-secondary" flex h-10 cursor-pointer items-center 
       justify-center rounded-lg border-2 border-light-primary font-archivoNarrow 
-      font-bold text-light-primary hover:bg-light-primary 
-      hover:text-light-background disabled:cursor-not-allowed dark:border-dark-primary dark:text-dark-primary dark:hover:bg-dark-primary
+      font-bold text-light-primary hover:bg-light-primary hover:text-light-background 
+      disabled:cursor-not-allowed 
+      dark:border-dark-primary dark:text-dark-primary dark:hover:bg-dark-primary
+      hover:dark:text-light-background 
       ${customStyles}`}
       {...props}
     >
