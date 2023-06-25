497acc2b4d61946757bbb41a0565728fd82376b2
Victor <victorlrmatos@gmail.com>
2023-06-25 18:43:16 -0300

    disabled button

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -4,12 +4,12 @@ export const Button = ({ label, onClick, customStyles, ...props }) => {
   return (
     <button
       onClick={onClick}
-      className={`dark:hover:text-dark-secondary" flex h-10 cursor-pointer items-center 
+      className={`flex h-10 cursor-pointer items-center 
       justify-center rounded-lg border-2 border-light-primary font-archivoNarrow 
       font-bold text-light-primary hover:bg-light-primary hover:text-light-background 
-      dark:border-dark-primary dark:text-dark-primary dark:hover:bg-dark-primary
-      hover:dark:text-light-background
-      ${customStyles} disabled:cursor-not-allowed disabled:opacity-50`}
+      disabled:pointer-events-none disabled:brightness-50 dark:border-dark-primary
+      dark:text-dark-primary dark:hover:bg-dark-primary hover:dark:text-light-background
+      ${customStyles} `}
       {...props}
     >
       {label}
