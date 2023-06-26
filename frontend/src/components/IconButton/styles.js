579b86acb17bea5d680b554989ae93c5f41b10f1
Pablo Felipe <pablofelipe1999@hotmail.com>
2023-06-26 16:34:43 -0300

    Needed to change button 'tooltip' style to allow it showing to the top (bottom navbar on mobile)

--- frontend/src/components/IconButton/styles.js
+++ frontend/src/components/IconButton/styles.js
@@ -2,7 +2,7 @@ export const styles = {
   icon: `relative mx-auto mb-2 mt-2 flex cursor-pointer items-center justify-center 
   transition duration-300 ease-in-out hover:scale-125`,
 
-  tooltip: `absolute left-10 w-auto min-w-max origin-left scale-0 
+  tooltip: `absolute -top-[56px] lg:-top-1 lg:left-10 w-auto min-w-max origin-left scale-0 
   rounded-md bg-light-primary p-2 text-xs font-bold 
   text-dark-secondary shadow-md transition-all duration-100`,
 };
